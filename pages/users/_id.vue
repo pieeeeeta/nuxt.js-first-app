b8ac4dea5f859e716a2a7dcece896a332eab3314
Takuho Tada <pieeeeeta@gmail.com>
2019-07-03 18:02:36 +0900

    titleを動的に変更

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -1,7 +1,24 @@
 <template>
-  <div>
-    <p>
-      /users/_id.vue
-    </p>
-  </div>
+  <section class="container">
+    <div>
+    </div>
+  </section>
 </template>
+
+<script>
+  export default {
+    head() {
+      return {
+        title: this.user.id
+      }
+    },
+    async asyncData({route, app}) {
+      const user = await app.$axios.$get(`https://qiita.com/api/v2/users/${route.params.id}`)
+      const items = 
+        await app.$axios.$get(`https://qiita.com/api/v2/items?query=user:${route.params.id}`)
+      return { user, items }
+
+    }
+  }
+
+</script>
